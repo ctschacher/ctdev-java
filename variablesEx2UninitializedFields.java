18e781baf9b966f9ab7fadf339da7a22d603c328
Christian Tschacher <ctschacher@axway.com>
2016-09-20 17:43:59 +0200

    add files for the chapter variables: exercise 1 and 2

---
+++ variablesEx2UninitializedFields.java
@@ -0,0 +1,9 @@
+public class variablesEx2UninitializedFields {
+	public static void main(String[] args) {
+		byte smallNumber = 112;
+		byte uninitialized;
+
+		System.out.println("smallNumber: " + smallNumber);
+		System.out.println("uninitialzed value: " + uninitialized);
+	}
+}
